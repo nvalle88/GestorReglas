94f7504cb0d0e4095f2a2c82328807728a7968ad
Néstor Yasmani Valle Rabelo <nestorvalle880416@gmail.com>
2019-09-17 17:33:03 -0500

    se actualiza newton soft 12.0.3

--- ConsoleApp2/Logica.cs
+++ ConsoleApp2/Logica.cs
@@ -22,26 +22,7 @@ namespace ConsoleApp2
         }
         public Logica()
         {
-            var salida = new SalidaGenerica { NombrePropiedad = "Nombre", Valor = "55", TipoDato = "string" };
-            var salidaCasa = new SalidaGenerica { NombrePropiedad = "Tamano", Valor = "20", TipoDato = "int" };
-
-
-
-            var gestor = new GestorReglas.LogicaNegocio.Gestor();
-
-            foreach (var item in gestor.L)
-            {
-
-                var tipo = item.GetType();
-                var PropertyInfos = item.GetType().GetProperties();
-                var propiedad = PropertyInfos.FirstOrDefault(x => x.Name == salida.NombrePropiedad);
-
-                if (propiedad != null)
-                {
-
-                    propiedad.SetValue(item, "This caption has been changed.", null);
-                }
-            }
+            
         }
     }
 }
