eee52398ff54121e22bb87aa841df74e38d983a5
Néstor Yasmani Valle Rabelo <nestorvalle880416@gmail.com>
2019-09-20 14:33:33 -0500

    reglas

---
+++ ConstructorRegla/Utiles/Signos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorRegla.Utiles
+{
+    public static class Signos
+    {
+        public static char Igual => '=';
+        public static char MayorQue => '>';
+        public static char MenorQue => '<';
+        public static string MayorIgualQue => ">=";
+        public static string MenorIgualQue => "<=";
+    }
+}
